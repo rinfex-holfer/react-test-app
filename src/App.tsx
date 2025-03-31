a741f40c76154722b433b205899087a35904abed
Gennady <greeny7greeny@gmail.com>
2025-03-31 19:01:38 +0200

    feat: add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Routes } from "react-router";
 import { Route } from "react-router";
 import { HomePage } from "./components/HomePage";
 import { routes } from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path={routes.home} element={<HomePage />} />
       </Routes>
-    </>
+    </QueryClientProvider>
   );
 }
 
